5284ab9cfb705f19697156691f61e433235011fb
Ramesh Babu Battini <rameshbabubattini@gmail.com>
2021-06-05 22:57:00 +0530

    Initial Commit

---
+++ src/main/java/com/azure/azurestoragedemo/AzureStorageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.azure.azurestoragedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureStorageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureStorageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/azurestoragedemo/AzureStorageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.azurestoragedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureStorageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
